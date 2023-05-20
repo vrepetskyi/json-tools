327257c17583860fcd59653c747e561c1c4d6d39
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2023-05-20 10:52:46 +0200

    Made json validation stricter

--- src/main/java/put/ai/se/jsontools/core/JsonString.java
+++ src/main/java/put/ai/se/jsontools/core/JsonString.java
@@ -1,15 +1,25 @@
 package put.ai.se.jsontools.core;
 
-import javafx.scene.control.Alert;
-
-import java.io.EOFException;
 import java.io.IOException;
+import java.io.StringReader;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
 
 public class JsonString implements JsonFormattable {
     private String value;
 
     public static boolean isValid(String value) {
-        return JsonValidator.isJsonValid(value);
+        try {
+            try (JsonReader reader = new JsonReader(new StringReader(value))) {
+                new Gson().getAdapter(JsonObject.class).read(reader);
+                reader.hasNext();
+                return true;
+            }
+        } catch (IOException e) {
+            return false;
+        }
     }
 
     public void setValue(String value) throws IllegalArgumentException {

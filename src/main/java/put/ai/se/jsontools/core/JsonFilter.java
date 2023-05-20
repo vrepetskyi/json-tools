8a8a916fc8fc6e6fd535ea69c2a0bfd2f21b196a
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2023-05-20 12:42:05 +0200

    Refactored

--- src/main/java/put/ai/se/jsontools/core/JsonFilter.java
+++ src/main/java/put/ai/se/jsontools/core/JsonFilter.java
@@ -5,30 +5,28 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class JsonFilter extends JsonFormattableDecorator {
-    
+
     public JsonFilter(JsonFormattable source) {
         super(source);
     }
 
     @Override
     public String getValue(JsonFormatParams params) {
+        String value = super.getValue(params);
 
-        String value = super.getValue(params); 
         Gson gson = new Gson();
         JsonElement jsonElement = gson.fromJson(value, JsonElement.class);
         JsonElement clone = JsonParser.parseString(jsonElement.toString());
 
-        if (params.getFilterMode() == FilterMode.Include) {
+        if (params.getFilterMode() == JsonFilterMode.Include) {
             Set<String> keys = new HashSet<>(clone.getAsJsonObject().keySet());
-            for (String entry: keys) {
+            for (String entry : keys) {
                 if (!params.getFilterKeys().contains(entry)) {
                     clone.getAsJsonObject().remove(entry);
                 }
             }
             value = gson.toJson(clone);
-        } 
-        else if (params.getFilterMode() == FilterMode.Exclude)
-        {
+        } else if (params.getFilterMode() == JsonFilterMode.Exclude) {
             for (String key : params.getFilterKeys()) {
                 if (clone.getAsJsonObject().has(key)) {
                     clone.getAsJsonObject().remove(key);
@@ -36,9 +34,9 @@ public class JsonFilter extends JsonFormattableDecorator {
             }
             value = gson.toJson(clone);
         } else {
-            System.out.println("Invalid filter mode"); // it's rather impossible to get to this part of code
+            throw new IllegalArgumentException("Invalid filter mode");
         }
-        
+
         return value;
     }
 }

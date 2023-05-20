ffd2710bf70d2968cb219d51c694ddf3d88df541
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2023-05-21 00:50:11 +0200

    Configured CI

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,5 @@
-module put.ai.se.jsontools {
+open module put.ai.se.jsontools {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
-
-    opens put.ai.se.jsontools.gui to javafx.fxml;
-
-    exports put.ai.se.jsontools;
-    exports put.ai.se.jsontools.core;
-    // exports put.ai.se.jsontools.api;
-    exports put.ai.se.jsontools.gui;
 }

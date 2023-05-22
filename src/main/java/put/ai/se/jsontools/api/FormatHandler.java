d06de6146a4c1d2adad90d374d867757436df0b1
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2023-05-22 18:56:46 +0200

    Created API structure

---
+++ src/main/java/put/ai/se/jsontools/api/FormatHandler.java
@@ -0,0 +1,11 @@
+package put.ai.se.jsontools.api;
+
+import java.io.IOException;
+
+import com.sun.net.httpserver.HttpExchange;
+
+public class FormatHandler {
+    public static void handle(HttpExchange exchange) throws IOException {
+        // TODO
+    }
+}

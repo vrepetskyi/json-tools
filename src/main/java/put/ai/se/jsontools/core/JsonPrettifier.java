376284a4923b96a125b5f9d75c7424e39a78dc3e
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2023-05-22 18:02:23 +0200

    Implemented JsonPrettifier

---
+++ src/main/java/put/ai/se/jsontools/core/JsonPrettifier.java
@@ -0,0 +1,21 @@
+package put.ai.se.jsontools.core;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+public class JsonPrettifier extends JsonFormattableDecorator {
+    public JsonPrettifier(JsonFormattable source) {
+        super(source);
+    }
+
+    @Override
+    public String getValue(JsonFormatParams params) {
+        String value = super.getValue(params);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonElement je = JsonParser.parseString(value);
+        String prettyJsonString = gson.toJson(je);
+        return prettyJsonString;
+    }
+}

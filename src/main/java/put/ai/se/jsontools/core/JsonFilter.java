0457e6c3ae5e3078381cb824e2135092d2f1155e
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2023-05-22 18:14:40 +0200

    Implemented JsonFilter

--- src/main/java/put/ai/se/jsontools/core/JsonFilter.java
+++ src/main/java/put/ai/se/jsontools/core/JsonFilter.java
@@ -1,5 +1,10 @@
 package put.ai.se.jsontools.core;
 
+import java.util.LinkedHashSet;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 public class JsonFilter extends JsonFormattableDecorator {
     public JsonFilter(JsonFormattable source) {
         super(source);
@@ -8,8 +13,33 @@ public class JsonFilter extends JsonFormattableDecorator {
     @Override
     public String getValue(JsonFormatParams params) {
         String value = super.getValue(params);
-        // TODO: process value in a proper way
-        return value;
-    }
 
+        JsonFilterParams filter = params.getFilter();
+        if (filter == null)
+            return value;
+
+        Gson gson = new Gson();
+        JsonObject filtered = gson.fromJson(value, JsonObject.class);
+
+        LinkedHashSet<String> filterKeys = filter.getKeys();
+        if (filterKeys == null)
+            return filter.getExclude() ? value : "{}";
+
+        LinkedHashSet<String> sourceKeys = new LinkedHashSet<>(filtered.keySet());
+        if (filter.getExclude()) {
+            for (String sourceKey : sourceKeys) {
+                if (filterKeys.contains(sourceKey)) {
+                    filtered.remove(sourceKey);
+                }
+            }
+        } else {
+            for (String sourceKey : sourceKeys) {
+                if (!filterKeys.contains(sourceKey)) {
+                    filtered.remove(sourceKey);
+                }
+            }
+        }
+
+        return gson.toJson(filtered);
+    }
 }

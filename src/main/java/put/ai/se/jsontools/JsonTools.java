b973127e3d1927162c37cdac3ea4aba7fb6c5f7a
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2023-05-22 16:46:17 +0200

    Extended logging

--- src/main/java/put/ai/se/jsontools/JsonTools.java
+++ src/main/java/put/ai/se/jsontools/JsonTools.java
@@ -1,20 +1,25 @@
 package put.ai.se.jsontools;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import put.ai.se.jsontools.api.ApiController;
 import put.ai.se.jsontools.gui.GuiController;
 
 public class JsonTools {
+    private static final Logger logger = LoggerFactory.getLogger(JsonTools.class);
+
     public static void main(String[] args) {
         try {
             new Thread(new ApiController()).start();
         } catch (Throwable e) {
-            // TODO: log api start error
+            logger.error("Error in ApiController", e);
         }
 
         try {
             new Thread(new GuiController()).start();
         } catch (Throwable e) {
-            // TODO: log gui start error
+            logger.error("Error in GuiController", e);
         }
     }
 }

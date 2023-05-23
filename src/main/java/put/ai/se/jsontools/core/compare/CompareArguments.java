600c87d7977b765d8e80543ae50aa04f92c56535
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2023-05-23 14:04:08 +0200

    Opted out the builder

--- src/main/java/put/ai/se/jsontools/core/compare/CompareArguments.java
+++ src/main/java/put/ai/se/jsontools/core/compare/CompareArguments.java
@@ -1,19 +1,31 @@
 package put.ai.se.jsontools.core.compare;
 
 public class CompareArguments {
-    protected String string1;
-    protected String string2;
-    protected boolean returnIdentical;
+    private String string1;
+    private String string2;
+    private boolean returnIdentical;
 
     public String getString1() {
         return string1;
+    }
+
+    public void setString1(String value) {
+        string1 = value;
     }
 
     public String getString2() {
         return string2;
     }
 
-    public boolean getReturnIdentical() {
+    public void setString2(String value) {
+        string2 = value;
+    }
+
+    public boolean isReturnIdentical() {
         return returnIdentical;
+    }
+
+    public void setReturnIdentical(boolean value) {
+        returnIdentical = value;
     }
 }

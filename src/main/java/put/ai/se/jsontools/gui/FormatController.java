3e9b0114fcfd60b0626cea65e778d557312683b8
KAndrukh <128008876+KAndrukh@users.noreply.github.com>
2023-05-23 16:07:46 +0200

    Made format GUI functional (#26)

--- src/main/java/put/ai/se/jsontools/gui/FormatController.java
+++ src/main/java/put/ai/se/jsontools/gui/FormatController.java
@@ -1,18 +1,38 @@
 package put.ai.se.jsontools.gui;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import put.ai.se.jsontools.core.JsonFormatParamsBuilder;
 import put.ai.se.jsontools.core.JsonFormatter;
-
-import java.io.IOException;
 
 public class FormatController {
     @FXML
     TextArea source;
     @FXML
     TextArea result;
+    @FXML
+    ToggleGroup groupPrettify;
+    @FXML
+    RadioButton prettify;
+    @FXML
+    RadioButton minify;
+    @FXML
+    ToggleGroup groupExclude;
+    @FXML
+    RadioButton exclude;
+    @FXML
+    RadioButton include;
+    @FXML
+    TextField keysTextField;
 
     @FXML
     public void initialize() {
@@ -23,7 +43,14 @@ public class FormatController {
     public void format() {
         result.setText("");
 
+        List<String> separated = Arrays.asList(keysTextField.getText().split("\\s*,\\s*"));
+        LinkedHashSet<String> keys = new LinkedHashSet<>(separated);
+
         JsonFormatParamsBuilder params = new JsonFormatParamsBuilder();
+
+        params.setFilterKeys(keys);
+        params.setExclude(exclude.isSelected());
+        params.setPrettify(prettify.isSelected());
 
         try {
             result.setText(JsonFormatter.format(source.getText(), params));
